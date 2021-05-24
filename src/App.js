4a115bf0b298860c2e45b3337f05fcef7ea0dcb6
srujan <srujansarode@protonmail.com>
2021-05-24 18:03:15 +0530

    Added route functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      hi
-    </div>
-  );
+	return (
+		<Switch>
+			<Route exact path="/">
+				This Is Home Page
+			</Route>
+			<Route exact path="/start">
+				This Is Start
+			</Route>
+			<Route>
+				<h2>404</h2>
+			</Route>
+		</Switch>
+	);
 }
 
 export default App;
