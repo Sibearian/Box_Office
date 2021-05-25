94d05a4d6b26b8c4b399b4a7d1ed1c7fb4c1f57e
srujan <srujansarode@protonmail.com>
2021-05-25 10:17:52 +0530

    added a search bar faster than stadia that fetches data from tv maza api

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../Components/MainPageLayout';
 
 const Home = () => {
-	return <MainPageLayout >This Is home</MainPageLayout>;
+	const [input, setInput] = useState('');
+
+	const onInputChange = ev => {
+		setInput(ev.target.value);
+	};
+
+	const onSearch = () => {
+		fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+			.then(r => r.json())
+			.then(r => {
+				console.log(r);
+			});
+	};
+
+	const onEnterKeyDown = event => {
+		if (event.keyCode === 13)
+		{
+			onSearch()
+		}
+	};
+
+	return (
+		<MainPageLayout>
+			<input
+				type="text"
+				onChange={onInputChange}
+				onKeyDown={onEnterKeyDown}
+				value={input}
+			/>
+			<button type="button" onClick={onSearch}>
+				Search
+			</button>
+		</MainPageLayout>
+	);
 };
 
 export default Home;
