a49641cacaf1a2aa09353f6a23e528aeec3ff919
srujan <srujansarode@protonmail.com>
2021-05-26 12:10:31 +0530

    added a radio buttton and search for shows or actors

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,14 +4,19 @@ import { apiGet } from '../Misc/Config';
 
 const Home = () => {
 	const [input, setInput] = useState('');
+	const [searchOption, setSearchOption] = useState('shows');
 	const [results, setResults] = useState(null);
+
+	const isShowChecked = searchOption === 'shows';
 
 	const onInputChange = ev => {
 		setInput(ev.target.value);
 	};
 
 	const onSearch = () => {
-		apiGet(`/search/shows?q=${input}`).then(result => setResults(result));
+		apiGet(`/search/${searchOption}?q=${input}`).then(result =>
+			setResults(result)
+		);
 	};
 
 	const onEnterKeyDown = event => {
@@ -26,25 +31,54 @@ const Home = () => {
 		}
 
 		if (results && results.length >= 0) {
-			return (
-				<div>
-					{results.map(item => {
-						return <div key={item.show.id}> {item.show.name} </div>;
-					})}
-				</div>
-			);
+			return results[0].show
+				? results.map(item => <div key={item.show.id}> {item.show.name} </div>)
+				: results.map(item => (
+						<div key={item.person.id}> {item.person.name} </div>
+				  ));
+
+			// results.map(item => {
+			// 	return <div key={item[0].id}> {item[0].name} </div>;
+			// })
 		}
 		return null;
+	};
+
+	const onRadioChange = event => {
+		setSearchOption(event.target.value);
 	};
 
 	return (
 		<MainPageLayout>
 			<input
 				type="text"
+				placeholder="Search for something"
 				onChange={onInputChange}
 				onKeyDown={onEnterKeyDown}
 				value={input}
 			/>
+			<div>
+				<lable htmlFor="shows-search">
+					shows
+					<input
+						type="radio"
+						id="shows-search"
+						value="shows"
+						onChange={onRadioChange}
+						checked={isShowChecked}
+					/>
+				</lable>
+				<lable htmlFor="actors-search">
+					Actors
+					<input
+						type="radio"
+						id="actors-search"
+						value="people"
+						onChange={onRadioChange}
+						checked={!isShowChecked}
+					/>
+				</lable>
+			</div>
 			<button type="button" onClick={onSearch}>
 				Search
 			</button>
