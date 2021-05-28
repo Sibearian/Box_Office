8c6b016d70771501471d9f8579f5acade03745d5
srujan <srujansarode@protonmail.com>
2021-05-28 21:38:24 +0530

    Installed styled-components and styled show and actor card using it

--- src/Components/Actor/ActorGrid.js
+++ src/Components/Actor/ActorGrid.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+
 import IMAGE_NOT_FOUND from '../../Images/not-found.png';
+import { FlexGrid } from '../Styled';
 import ActorCard from './ActorCard';
 
 const ActorGrid = ({ data }) => {
 	return (
-		<div>
+		<FlexGrid>
 			{data.map(({ person }) => (
 				<ActorCard
 					key={person.id}
@@ -16,7 +18,7 @@ const ActorGrid = ({ data }) => {
 					name={person.name.replace(/\./gi, '')}
 				/>
 			))}
-		</div>
+		</FlexGrid>
 	);
 };
 
