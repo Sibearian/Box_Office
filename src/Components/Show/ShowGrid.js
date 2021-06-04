8b607409245c04c8f89b2a8ba307089e81822dfb
srujan <srujansarode@protonmail.com>
2021-06-04 14:22:13 +0530

    Added working starred page

--- src/Components/Show/ShowGrid.js
+++ src/Components/Show/ShowGrid.js
@@ -2,20 +2,37 @@ import React from 'react';
 import ShowCard from './ShowCard';
 import { FlexGrid } from '../Styled';
 
+import { useShows } from '../../Misc/costom-hooks';
 import IMAGE_NOT_FOUND from '../../Images/not-found.png';
 
 const ShowGrid = ({ data }) => {
+	const [starredShows, dispatchStarred] = useShows();
+
 	return (
 		<FlexGrid>
-			{data.map(({ show }) => (
-				<ShowCard
-					key={show.id}
-					id={show.id}
-					name={show.name}
-					image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
-					summary={show.summary}
-				/>
-			))}
+			{data.map(({ show }) => {
+				const isStarred = starredShows.includes(show.id);
+
+				const onStarred = () => {
+					if (isStarred) {
+						dispatchStarred({ type: 'REMOVE', showId: show.id });
+					} else {
+						dispatchStarred({ type: 'ADD', showId: show.id });
+					}
+				};
+
+				return (
+					<ShowCard
+						key={show.id}
+						id={show.id}
+						name={show.name}
+						image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
+						summary={show.summary}
+						onClick={onStarred}
+						isActive={isStarred}
+					/>
+				);
+			})}
 		</FlexGrid>
 	);
 };
--- src/Components/Show/ShowMainData.js
+++ src/Components/Show/ShowMainData.js
@@ -1,34 +1,37 @@
-import React from 'react'
-import {Star} from '../Styled'
+import React from 'react';
+import { Star } from '../Styled';
 
 import IMAGE_NOT_FOUND from '../../Images/not-found.png';
 import { Headline, MainDataWrapper, TagList } from './ShowMainData.styled';
 
 const ShowMainData = ({ name, rating, summary, tags, image }) => {
-  return (
-    <MainDataWrapper>
-      <img src={image ? image.original : IMAGE_NOT_FOUND} alt="show-cover" />
-      <div className="text-side">
-        <Headline>
-          <h1>{name}</h1>
-          <div>
-            <Star />
-            <span>{rating.average || 'N/A'}</span>
-          </div>
-        </Headline>
-        <div className="summary" dangerouslySetInnerHTML={{ __html: summary }} />
+	return (
+		<MainDataWrapper>
+			<img src={image ? image.original : IMAGE_NOT_FOUND} alt="show-cover" />
+			<div className="text-side">
+				<Headline>
+					<h1>{name}</h1>
+					<div>
+						<Star isActive />
+						<span>{rating.average || 'N/A'}</span>
+					</div>
+				</Headline>
+				<div
+					className="summary"
+					dangerouslySetInnerHTML={{ __html: summary }}
+				/>
 
-        <div>
-          Tags:{' '}
-          <TagList>
-            {tags.map((tag, i) => (
-              <span key={i}>{tag}</span>
-            ))}
-          </TagList>
-        </div>
-      </div>
-    </MainDataWrapper>
-  );
+				<div>
+					Tags: {' '}
+					<TagList>
+						{tags.map((tag, i) => (
+							<span key={i}>{tag}</span>
+						))}
+					</TagList>
+				</div>
+			</div>
+		</MainDataWrapper>
+	);
 };
 
-export default ShowMainData
+export default ShowMainData;
